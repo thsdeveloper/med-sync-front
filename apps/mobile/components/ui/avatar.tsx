9e1eb20e63270b61186bc46763fd8675a7deeb61
ThiagoSoares <ths.pereira@gmail.com>
2025-12-19 13:38:13 -0300

    Correções de bug de implementação.

--- apps/mobile/components/ui/avatar.tsx
+++ apps/mobile/components/ui/avatar.tsx
@@ -56,6 +56,8 @@ const styles = StyleSheet.create({
     overflow: 'hidden', // Ensure image respects border radius
   },
   image: {
+    width: '100%',
+    height: '100%',
     borderRadius: 999,
   },
   imageFull: {
